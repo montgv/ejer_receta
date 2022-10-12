d75614e18afa524e4271375dc3ebf6d751dcc65e
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-12 22:13:11 +0200

    Initial commit

---
+++ src/main/java/Receta.java
@@ -0,0 +1,17 @@
+public class Receta {
+
+    private String tipo;
+    private String dificultad;
+    private String nombre;
+    private String[] ingredientes;
+    private String[] ingrediente;
+    private String nombreIngrediente;
+    private String cantidad;
+    private int calorias;
+    private String[] pasos;
+    private String paso;
+    private String tiempo;
+    private String elaboracion;
+
+
+}
